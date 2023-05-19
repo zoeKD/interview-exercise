c7169ccebcbb9ab780ca96c5b7af50178832cc3d
pdelange <pascal.delange@polytechnique.org>
2023-05-19 09:31:37 +0200

    move+comment

--- data_model.go
+++ data_model.go
@@ -23,3 +23,22 @@ type Field struct {
 	Name     FieldName
 	DataType DataType
 }
+
+func getDataModel() Table {
+	// In practice, this would be retrieved dynamically from a database
+	return Table{
+		Name: "transactions",
+		Fields: map[FieldName]Field{
+			"object_id":   {Name: "object_id", DataType: String},
+			"updated_at":  {Name: "updated_at", DataType: Timestamp},
+			"amount":      {Name: "amount", DataType: Int},
+			"account_id":  {Name: "account_id", DataType: String},
+			"bic_country": {Name: "bic_country", DataType: String},
+			"country":     {Name: "country", DataType: String},
+			"description": {Name: "description", DataType: String},
+			"direction":   {Name: "direction", DataType: String},
+			"status":      {Name: "status", DataType: String},
+			"title":       {Name: "title", DataType: String},
+		},
+	}
+}
--- main.go
+++ main.go
@@ -22,21 +22,7 @@ func main() {
 	defer conn.Close(context.Background())
 	err = setupDbSchema(conn)
 
-	testDataModel := Table{
-		Name: "transactions",
-		Fields: map[FieldName]Field{
-			"object_id":   {Name: "object_id", DataType: String},
-			"updated_at":  {Name: "updated_at", DataType: Timestamp},
-			"amount":      {Name: "amount", DataType: Int},
-			"account_id":  {Name: "account_id", DataType: String},
-			"bic_country": {Name: "bic_country", DataType: String},
-			"country":     {Name: "country", DataType: String},
-			"description": {Name: "description", DataType: String},
-			"direction":   {Name: "direction", DataType: String},
-			"status":      {Name: "status", DataType: String},
-			"title":       {Name: "title", DataType: String},
-		},
-	}
+	testDataModel := getDataModel()
 	fmt.Printf("Datamodel to use: %+v\n", testDataModel)
 
 	r := chi.NewRouter()
